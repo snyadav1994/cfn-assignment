9b9e447858636beb95901c95a6d2ccd3e237eca7
Sneha Yadav <syadav@tavisca.com>
2018-08-08 17:53:25 +0530

    second commit

--- ImageCleanup.py
+++ ImageCleanup.py
@@ -2,9 +2,7 @@ import boto3
 from botocore.exceptions import ClientError
 import sys
 
-#aws_access_key = "AKIAIFAHNMXIAQAYSO3A"
-#aws_secret_key = "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"
-#region = "us-east-1"
+
 role_arn = "arn:aws:iam::346319152574:role/bot_ec2_reporting"
 role_session_name = "temporarySession"
 
@@ -13,7 +11,7 @@ role_session_name = "temporarySession"
 sts_client = boto3.client('sts')
 try :
     response = sts_client.assume_role(RoleArn=role_arn, RoleSessionName=role_session_name)
-    #print (response)
+    print (response)
 
 except ClientError as e :
     error_code = e.response['Error']['Code']

e2c973f096fc004712a162fa496fb54d127b9ed4
Sneha Yadav <syadav@tavisca.com>
2018-08-14 15:02:28 +0530

    15 commit

--- ImageCleanup.py
+++ ImageCleanup.py
@@ -16,7 +16,8 @@ credentials = response['Credentials']
 aws_access_key_id=credentials['AccessKeyId'],
 aws_secret_access_key=credentials['SecretAccessKey'],
 aws_session_token=credentials['SessionToken']
-   
+
+boto3.setup_default_session(role_session_name='temporarySession')   
 ec2_client = boto3.client('ec2',region_name = "us-east-1")
 res = ec2_client.describe_images(
             ImageIds = ['ami-00312917',]

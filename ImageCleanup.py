621cd405599043a76177a15f05ec88dbfc598591
Sneha Yadav <syadav@tavisca.com>
2018-08-14 20:11:21 +0530

    23 commit

--- ImageCleanup.py
+++ ImageCleanup.py
@@ -5,6 +5,6 @@ import sys
    
 ec2_client = boto3.client('ec2',region_name="us-east-1")
 res = ec2_client.describe_images(
-            ImageIds = ['ami-00312917',]
+            ImageIds = ['ami-2f796554',]
         )
 print (res['Images'])

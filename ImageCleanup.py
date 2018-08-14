08018058b0e69d24df9c94d40089318e772989e4
Sneha Yadav <syadav@tavisca.com>
2018-08-14 20:09:59 +0530

    22 commit

--- ImageCleanup.py
+++ ImageCleanup.py
@@ -3,7 +3,7 @@ from botocore.exceptions import ClientError
 import sys
 
    
-ec2_client = boto3.client('ec2')
+ec2_client = boto3.client('ec2',region_name="us-east-1")
 res = ec2_client.describe_images(
             ImageIds = ['ami-00312917',]
         )

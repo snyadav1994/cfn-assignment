afabd965a2ec15d51abf6423e389cc48e536fec7
Sneha Yadav <syadav@tavisca.com>
2018-08-14 19:59:07 +0530

    21 commit

--- ImageCleanup.py
+++ ImageCleanup.py
@@ -2,22 +2,8 @@ import boto3
 from botocore.exceptions import ClientError
 import sys
 
-
-role_arn = "arn:aws:iam::346319152574:role/travel-qa-deployment-role"
-role_session_name = "temporarySession"
-
-
-
-sts_client = boto3.client('sts',region_name = "us-east-1")
-
-response = sts_client.assume_role(RoleArn=role_arn, RoleSessionName=role_session_name)
-    #print (response)
-credentials = response['Credentials']
-aws_access_key_id=credentials['AccessKeyId'],
-aws_secret_access_key=credentials['SecretAccessKey'],
-aws_session_token=credentials['SessionToken']
    
-ec2_client = boto3.client('ec2',aws_access_key_id,aws_secret_access_key,aws_session_token)
+ec2_client = boto3.client('ec2')
 res = ec2_client.describe_images(
             ImageIds = ['ami-00312917',]
         )
